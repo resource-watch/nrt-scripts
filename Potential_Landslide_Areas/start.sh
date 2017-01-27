08d37edb23b909e3b78ee2c92cf9fe4d59ac5e33
Alicia <alicia.arenzana@gmail.com>
2017-01-27 12:37:07 +0100

    added docker to landslides

---
+++ Potential_Landslide_Areas/start.sh
@@ -0,0 +1,4 @@
+#Change the NAME variable with the name of your script
+NAME=my-first-script
+docker build -t $NAME --build-arg NAME=$NAME .
+docker run -v $(pwd)/data:/opt/$NAME/data --env-file .env --rm $NAME
