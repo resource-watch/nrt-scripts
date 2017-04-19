c6e22016b3ce8afeca01323c3e6b1326931ee91b
Alicia <alicia.arenzana@gmail.com>
2017-04-19 11:43:42 +0200

    updated netcdf lib

--- coralReefs_hotspots/src/__init__.py
+++ coralReefs_hotspots/src/__init__.py
@@ -1,13 +1,39 @@
+from __future__ import print_function, division
 import wget
 import os
-import requests
+import sys
+import threading
 import datetime
 from netCDF4 import Dataset
 import numpy as np
 import rasterio
-import tinys3
+import boto3
 from rasterio.transform import from_origin
 
+class bcolors:
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ENDC = '\033[0m'
+    UNDERLINE = '\033[4m'
+
+class ProgressPercentage(object):
+    def __init__(self, filename):
+        self._filename = filename
+        self._size = float(os.path.getsize(filename))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+    def __call__(self, bytes_amount):
+        # To simplify we'll assume this is hooked up
+        # to a single filename.
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%)" % (
+                    self._filename, self._seen_so_far, self._size,
+                    percentage))
+            sys.stdout.flush()
 
 # Download last dataset (2 days in the past)
 
@@ -15,6 +41,7 @@ def dataDownload():
     today = datetime.date.today()
     url='ftp://ftp.star.nesdis.noaa.gov/pub/sod/mecb/crw/data/5km/nc/baa_max_comp_7day/2017/baa_max_r07d_b05kmnn_'+ str(today.year) +"%02d" % (today.month)+"%02d" % (today.day-2)+'.nc'
     filename = wget.download(url)
+    print('data download finish')
     return filename
 
 
@@ -46,28 +73,41 @@ def netcdf2tif(dst,outFile):
         'nodata':nc['CRW_BAA_max7d']._FillValue
     }
     with rasterio.open(outFile, 'w', **profile) as dst:
-        dst.write(data.astype(profile['dtype']), 1)  
+        dst.write(data.astype(profile['dtype']), 1)
+    print('transformation finish') 
 
 
 # S3 upload
 def s3Upload(outFile):
-    # Push to Amazon S3 instance
-    conn = tinys3.Connection(os.getenv('S3_ACCESS_KEY'),
-                             os.getenv('S3_SECRET_KEY'), 
-                             tls=True)
-    # So we could skip the bucket parameter on every request
-    f = open(outFile, 'rb')
-    response = conn.upload(outFile, f, os.getenv('BUCKET'))
-    if response.status_code == 200:
-        print(Fore.GREEN + '\r SUCCESS')
-    else:
-        print(Fore.RED + 'UPLOAD PROCESS FAILURE STATUS CODE: {0}'.format(
-            str(response.status_code)))
-        print('\r {0}'.format(str(response.content)))
+    # Get the service client
+    s3 = boto3.client('s3', aws_access_key_id=os.getenv('S3_ACCESS_KEY'), aws_secret_access_key=os.getenv('S3_SECRET_KEY'))
+    print('s3 connection open; proceeded to upload data...') 
+    # Upload a file-like object to bucket-name at key-name
+    try:
+        with open(outFile, "rb") as f:
+            response = s3.upload_fileobj(f, os.getenv('BUCKET'), outFile, ExtraArgs={'ACL': 'public-read'}, Callback=ProgressPercentage(outFile))
+            print(response)
+        
+    except Exception as error:
+        print(error)
+    
+    #s3._endpoint.http_session.close()
+
+    # conn = tinys3.Connection(os.getenv('S3_ACCESS_KEY'),os.getenv('S3_SECRET_KEY'), tls=True, default_bucket=os.getenv('BUCKET'), endpoint="s3.amazonaws.com")
+    # # So we could skip the bucket parameter on every request
+    # print('r')
+    # print(conn)
+    # response = conn.upload(key=outFile, local_file=open(outFile,'rb'), public=True, close=True)
+    # print('rffffff')
+
+    # if response.status_code==200:
+    #     print('SUCCESS')
+    # else:
+    #     print('UPLOAD PROCESS FAILURE STATUS CODE:' + str(response.status_code))
+    #     print(response.content)
 
 
 # Execution
-
 outFile ='CoralReefHotspots.tif'
 file = dataDownload()
 netcdf2tif(file,outFile)
