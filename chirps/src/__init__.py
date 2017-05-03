9274c2f62660678e5fb917862ae0d97b28d2f34f
Ronny De Abreu <ronnydeabreu@gmail.com>
2017-05-03 13:23:50 +0200

    Tropical and Temperate Rainfall Dataset, Climate Hazard InfraRed Precipitation with Stations: CHIRPS (USGS, USAID, NASA, NOAA)

--- chirps/src/__init__.py
+++ chirps/src/__init__.py
@@ -1,72 +1,86 @@
-from __future__ import print_function, division
-import wget
-import os
-import sys
-import threading
-import datetime
-from netCDF4 import Dataset
+import pandas as pd
 import numpy as np
+from os.path import basename, dirname, exists
+import os
 import rasterio
+import glob
+import ftplib
+import urllib2
+import gzip
+import shutil
+from contextlib import closing
+from netCDF4 import Dataset
+
+mypath = "/Users/vizzuality/Documents/Vizzuality/RW/planet_pulse/nrt-scripts/"
+new_dir = str(mypath)+'chirps'
+!mkdir $new_dir
+
+
+listing = []
+response = urllib2.urlopen('ftp://chg-ftpout.geog.ucsb.edu/pub/org/chg/products/CHIRPS-2.0/global_daily/tifs/p05/2017/')
+for line in response:
+    listing.append(line.rstrip())
+
+s2=pd.DataFrame(listing)
+s3=s2[0].str.split()
+s4=s3[len(s3)-1]
+last_file = s4[8]
+
+uncompressed = os.path.splitext(last_file)[0]
+
+with closing(urllib2.urlopen('ftp://chg-ftpout.geog.ucsb.edu/pub/org/chg/products/CHIRPS-2.0/global_daily/tifs/p05/2017/'+str(last_file))) as r:
+    with open(str(last_file), 'wb') as f:
+        shutil.copyfileobj(r, f)
+
+!gzip -d $last_file > $new_dir #uncompress my file raster
+!gdalinfo $uncompressed 
+
+src = rasterio.open(str(mypath)+str(uncompressed))
+
+array = src.read(1)
+
+with rasterio.open(str(mypath)+str(uncompressed)) as src:
+    npixels = src.width * src.height
+    for i in src.indexes:
+        band = src.read(i)
+        print(i, band.min(), band.max(), band.sum()/npixels)
+
+CM_IN_FOOT = 30.48
+
+with rasterio.drivers():
+    with rasterio.open(str(mypath)+str(uncompressed)) as src:
+        kwargs = src.meta
+        kwargs.update(
+            driver='GTiff',
+            dtype=rasterio.float64,  #rasterio.int16, rasterio.int32, rasterio.uint8,rasterio.uint16, rasterio.uint32, rasterio.float32, rasterio.float64
+            count=1,
+            compress='lzw',
+            nodata=0,
+            bigtiff='YES' # Output will be larger than 4GB
+        )
+
+        windows = src.block_windows(1)
+
+        with rasterio.open(str(mypath)+str(uncompressed),'w',**kwargs) as dst:
+            for idx, window in windows:
+                src_data = src.read(1, window=window)
+
+                # Source nodata value is a very small negative number
+                # Converting in to zero for the output raster
+                np.putmask(src_data, src_data < 0, 0)
+
+                dst_data = (src_data * CM_IN_FOOT).astype(rasterio.float64)
+                dst.write_band(1, dst_data, window=window)
+
+!gdalinfo $uncompressed
+
 import tinys3
-from rasterio.transform import from_origin
-
-# Download last dataset (2 days in the past)
-
-def dataDownload(): 
-    today = datetime.date.today()
-    url='ftp://ftp.star.nesdis.noaa.gov/pub/sod/mecb/crw/data/5km/nc/baa_max_comp_7day/2017/baa_max_r07d_b05kmnn_'+ str(today.year) +"%02d" % (today.month)+"%02d" % (today.day-2)+'.nc'
-    filename = wget.download(url)
-    print('data download finish')
-    return filename
-
-
-# Convert nc to geotiff
-
-
-def netcdf2tif(dst,outFile):
-    nc = Dataset(dst)
-    data = nc['CRW_BAA_max7d'][0,:,:].squeeze()
-    # Return lat info
-    south_lat = nc.geospatial_lat_min - nc.geospatial_lat_resolution/2  # Change pos.to edges of pxls (not center)
-    north_lat = nc.geospatial_lat_max + nc.geospatial_lat_resolution/2
-
-    # Return lon info
-    west_lon = nc.geospatial_lon_min - nc.geospatial_lon_resolution/2
-    east_lon = nc.geospatial_lon_max + nc.geospatial_lon_resolution/2
-    # Transformation function
-    transform = rasterio.transform.from_bounds(west_lon, south_lat, east_lon, north_lat, data.shape[1], data.shape[0])
-    # Profile
-    profile = {
-        'driver':'GTiff', 
-        'height':data.shape[0], 
-        'width':data.shape[1], 
-        'count':1, 
-        'dtype':np.int16, 
-        'crs':'EPSG:4326', 
-        'transform':transform, 
-        'compress':'lzw', 
-        'nodata':nc['CRW_BAA_max7d']._FillValue
-    }
-    with rasterio.open(outFile, 'w', **profile) as dst:
-        dst.write(data.astype(profile['dtype']), 1)
-    print('transformation finish') 
-
-
-# S3 upload
-def s3Upload(outFile):
-    conn = tinys3.Connection(os.getenv('S3_ACCESS_KEY'),os.getenv('S3_SECRET_KEY'), tls=True, default_bucket=os.getenv('BUCKET'), endpoint="s3.amazonaws.com")
-    # So we could skip the bucket parameter on every request
-    response = conn.upload(key=outFile, local_file=open(outFile,'rb'), public=True, close=True)
-    if response.status_code==200:
-        print('SUCCESS')
-    else:
-        print('UPLOAD PROCESS FAILURE STATUS CODE:' + str(response.status_code))
-        print(response.content)
-
+conn = tinys3.Connection('S3_ACCESS_KEY','S3_SECRET_KEY',tls=True)
+f = open('./'+str(uncompressed),'rb')
+conn.upload('./'+str(uncompressed),f,'my_bucket')
 
 # Execution
-outFile ='CoralReefHotspots.tif'
+outFile ='chirps-v2.0.2017.03.31.tif'
 file = dataDownload()
 netcdf2tif(file,outFile)
 s3Upload(outFile)
-
