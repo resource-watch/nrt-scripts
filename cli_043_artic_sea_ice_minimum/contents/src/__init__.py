ac6401f2ac05493f1bebb1bb13039c17e89aefc7
nsuberi <nsuberi@gmail.com>
2017-12-26 17:25:10 -0500

    connect branch

---
+++ cli_043_artic_sea_ice_minimum/contents/src/__init__.py
@@ -0,0 +1,174 @@
+import logging
+import sys
+import os
+import urllib.request
+from collections import OrderedDict
+from datetime import datetime, timedelta
+import cartosql
+
+### Constants
+SOURCE_URL = 'https://climate.nasa.gov/system/internal_resources/details/original/'
+# One or both of these may not be needed
+DATETIME_INDEX = 0
+FILENAME_INDEX = 0
+
+### Table name and structure
+CARTO_TABLE = 'cli_043_arctic_sea_ice_minimum'
+CARTO_SCHEMA = OrderedDict([
+    ('area', 'numeric'),
+    ('date_area', 'timestamp'),
+    ('extent', 'numeric'),
+    ('date_extent', 'timestamp')])
+UID_FIELD_AREA = 'date_area'
+TIME_FIELD_AREA = 'date_area'
+UID_FIELD_EXTENT = 'date_extent'
+TIME_FIELD_EXTENT = 'date_extent'
+
+CARTO_USER = os.environ.get('CARTO_USER')
+CARTO_KEY = os.environ.get('CARTO_KEY')
+
+# Table limits
+MAX_ROWS = 1000000
+
+###
+## Accessing remote data
+###
+
+def fetchDataFileName(SOURCE_URL):
+    """
+    Select the appropriate file from FTP to download data from
+    """
+    with urllib.request.urlopen(SOURCE_URL) as f:
+        ftp_contents = f.read().decode('utf-8').splitlines()
+
+    filename = ''
+    ALREADY_FOUND=False
+    for fileline in ftp_contents:
+        fileline = fileline.split()
+        potential_filename = fileline[FILENAME_INDEX]
+        # This is specific to this FTP
+        if (potential_filename.endswith(".txt") and ("V4" in potential_filename)):
+            if not ALREADY_FOUND:
+                filename = potential_filename
+                ALREADY_FOUND=True
+            else:
+                logging.warning("There are multiple filenames which match criteria, passing most recent")
+                filename = potential_filename
+
+    logging.info("Selected filename: " + filename)
+    if not ALREADY_FOUND:
+        logging.warning("No valid filename found")
+
+    # Return the file name
+    return(filename)
+
+def processData(SOURCE_URL, filename, existing_ids):
+    """
+    Inputs: FTP SOURCE_URL and filename where data is stored, existing_ids not to duplicate
+    Actions: Retrives data, dedupes and formats it, and adds to Carto table
+    Output: Number of new rows added
+    """
+    with urllib.request.urlopen(os.path.join(SOURCE_URL, filename)) as f:
+        res_rows = f.read().decode('utf-8').splitlines()
+
+    # Do not keep header rows, or data observations marked 999
+    deduped_formatted_rows = []
+    for row in res_rows:
+
+        ###
+        ## CHANGE TO REFLECT CRITERIA FOR KEEPING ROWS FROM THIS DATA SOURCE
+        ###
+        if not (row.startswith("HDR")):
+            potential_row = row.split()
+            if len(potential_row)==len(CARTO_SCHEMA):
+
+                ###
+                ## ENSURE UPDATE DATE FUNCTION
+                ###
+
+                date = formatDateFunction(float(potential_row[DATETIME_INDEX]))
+                if date not in existing_ids:
+                    potential_row[DATETIME_INDEX] = date
+                    deduped_formatted_rows.append(potential_row)
+
+    logging.debug("First ten deduped, formatted rows from ftp: " + str(deduped_formatted_rows[:10]))
+
+    if len(deduped_formatted_rows):
+        cartosql.blockInsertRows(CARTO_TABLE, CARTO_SCHEMA, deduped_formatted_rows)
+
+    return(len(deduped_formatted_rows))
+
+###
+## Processing data for Carto
+###
+
+def formatDateFunction(unformatteddate):
+    # DO STUFF
+    formatted_date = unformatteddate
+    return(formatted_date)
+
+'''
+Options include:
+
+# https://stackoverflow.com/questions/20911015/decimal-years-to-datetime-in-python
+def decimalToDatetime(dec, date_pattern="%Y-%m-%d %H:%M:%S"):
+    """
+    Convert a decimal representation of a year to a desired string representation
+    I.e. 2016.5 -> 2016-06-01 00:00:00
+    """
+    year = int(dec)
+    rem = dec - year
+    base = datetime(year, 1, 1)
+    dt = base + timedelta(seconds=(base.replace(year=base.year + 1) - base).total_seconds() * rem)
+    result = dt.strftime(date_pattern)
+    return(result)
+'''
+
+###
+## Application code
+###
+
+def main():
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
+    ### 1. Check if table exists, if so, retrieve UIDs
+    ## Q: If not getting the field for TIME_FIELD, can you still order by it?
+    if cartosql.tableExists(CARTO_TABLE):
+        r = cartosql.getFields(UID_FIELD, CARTO_TABLE, order='{} desc'.format(TIME_FIELD), f='csv')
+        # quick read 1-column csv to list
+        logging.debug("Table detected")
+        logging.debug(r.text)
+        existing_ids = r.text.split('\r\n')[1:-1]
+
+    ### 2. If not, create table
+    else:
+        logging.info('Table {} does not exist'.format(CARTO_TABLE))
+        cartosql.createTable(CARTO_TABLE, CARTO_SCHEMA)
+        existing_ids = []
+
+    logging.debug("First 10 IDs already in table: " + str(existing_ids[:10]))
+
+    ### 3. Fetch data from FTP, dedupe, process
+    filename = fetchDataFileName(SOURCE_URL)
+    num_new_rows = processData(SOURCE_URL, filename, existing_ids)
+
+    ### 4. Remove old to make room for new
+    oldcount = len(existing_ids)
+    logging.info('Previous rows: {}, New rows: {}, Max: {}'.format(oldcount, num_new_rows, MAX_ROWS))
+
+    if oldcount + num_new_rows > MAX_ROWS:
+        if MAX_ROWS > len(new_rows):
+            # ids_already_in_table are arranged in increasing order
+            # Drop all except the most recent ones we have room to keep
+            drop_ids = existing_ids[(MAX_ROWS - len(new_rows)):]
+            drop_response = cartosql.deleteRowsByIDs(CARTO_TABLE, UID_FIELD, drop_ids)
+        else:
+            logging.warning("There are more new rows than can be accommodated in the table. All existing_ids were dropped")
+            drop_response = cartosql.deleteRowsByIDs(CARTO_TABLE, UID_FIELD, existing_ids)
+
+        numdropped = drop_response.json()['total_rows']
+        if numdropped > 0:
+            logging.info('Dropped {} old rows'.format(numdropped))
+
+    ###
+    logging.info("SUCCESS")
