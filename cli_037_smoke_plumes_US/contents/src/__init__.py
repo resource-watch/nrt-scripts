a817a1823f089c029669f4a89cfc95d07be5be5a
nsuberi <nsuberi@gmail.com>
2017-12-30 12:29:41 -0500

    add code to download from archive, increase max rows to 100000, set default to not download from archive anymore, look at past 10 days in the nrt folder

--- cli_037_smoke_plumes_US/contents/src/__init__.py
+++ cli_037_smoke_plumes_US/contents/src/__init__.py
@@ -6,18 +6,21 @@ import logging
 import sys
 import urllib
 from datetime import datetime, timedelta
-from collections import OrderedDict
+from collections import OrderedDict, deque
 import cartosql
 import zipfile
 
 # Constants
 DATA_DIR = 'data'
 SOURCE_URL = 'ftp://satepsanone.nesdis.noaa.gov/FIRE/HMS/GIS/hms_smoke{date}.zip'
+SOURCE_URL_ARCHIVE = 'ftp://satepsanone.nesdis.noaa.gov/FIRE/HMS/GIS/ARCHIVE/hms_smoke{date}.zip'
 FILENAME = 'hms_smoke{date}'
 TIMESTEP = {'days': 1}
 DATE_FORMAT = '%Y%m%d'
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 CLEAR_TABLE_FIRST = False
+LOG_LEVEL = logging.DEBUG
+MAX_CHECK = 10
 
 # asserting table structure rather than reading from input
 CARTO_TABLE = 'cli_037_smoke_plumes'
@@ -34,10 +37,9 @@ CARTO_SCHEMA = OrderedDict([
 UID_FIELD = '_UID'
 TIME_FIELD = 'date'
 
-LOG_LEVEL = logging.INFO
-MAXROWS = 10000
+MAXROWS = 100000
 MAXAGE = datetime.today() - timedelta(days=365*10)
-MAXAGE_UPLOAD = datetime.today() - timedelta(days=10)
+MAXAGE_UPLOAD = datetime.today() - timedelta(days=360)
 
 
 # Generate UID
@@ -56,7 +58,7 @@ def formatObservationDatetime(start, end, datetime_format=DATETIME_FORMAT):
     minute = int(time[-2:])
     start_dt = datetime(year=year,month=1,day=1) + timedelta(days=day, hours=hour, minutes=minute)
 
-    date, time = start.split(' ')
+    date, time = end.split(' ')
     year = int(date[:4])
     day = int(date[4:])-1 # Account for fact that we're initiating from day 1
     hour = int(time[:-2])
@@ -77,7 +79,7 @@ def findShp(zfile):
 
 def getNewDates(exclude_dates):
     '''Get new dates excluding existing'''
-    new_dates = []
+    new_dates = deque([])
     date = datetime.today()
     while date > MAXAGE_UPLOAD:
         date -= timedelta(**TIMESTEP)
@@ -90,24 +92,40 @@ def getNewDates(exclude_dates):
     return new_dates
 
 
-def processNewData(exclude_ids):
+def processNewData(exclude_ids, from_archive=False):
     new_ids = []
+    checked = 0
+    go = True
 
     # get non-existing dates
     dates = set([getDate(uid) for uid in exclude_ids])
     new_dates = getNewDates(dates)
-    for date in new_dates:
-        # 1. Fetch data from source
 
-        url = SOURCE_URL.format(date=date)
+    while len(new_dates) and go:
+        # Only look back MAX_CHECK days, unless reading in archive
+        if not from_archive and checked > MAX_CHECK:
+            go = False
+            continue
+
+        logging.debug('Dates left to check: {}'.format(len(new_dates)))
+
+        # https://stackoverflow.com/questions/4426663/how-do-i-remove-the-first-item-from-a-python-list
+        date = new_dates.popleft()
+        if from_archive:
+            url = SOURCE_URL_ARCHIVE.format(date=date)
+        else:
+            url = SOURCE_URL.format(date=date)
+
         tmpfile = '{}.zip'.format(os.path.join(DATA_DIR,
                                                FILENAME.format(date=date)))
+
         logging.info('Fetching {}'.format(date))
         try:
             urllib.request.urlretrieve(url, tmpfile)
         except Exception as e:
             logging.warning('Could not retrieve {}'.format(url))
             logging.error(e)
+            checked += 1
             continue
 
         # 2. Parse fetched data and generate unique ids
@@ -147,8 +165,9 @@ def processNewData(exclude_ids):
 
         # 4. Insert new observations
         new_count = len(rows)
+        checked += 1
         if new_count:
-            logging.info('Pushing new rows')
+            logging.info('Pushing {} new rows'.format(new_count))
             cartosql.insertRows(CARTO_TABLE, CARTO_SCHEMA.keys(),
                                 CARTO_SCHEMA.values(), rows)
     num_new = len(new_ids)
@@ -212,9 +231,12 @@ def main():
         createTableWithIndices(CARTO_TABLE, CARTO_SCHEMA, UID_FIELD, otherFields=[TIME_FIELD])
 
     # 2. Iterively fetch, parse and post new data
-    num_new = processNewData(existing_ids)
+    num_new = processNewData(existing_ids, from_archive=False)
+    #num_new_from_archive = processNewData(existing_ids, from_archive=True)
+    logging.debug('Num new: {}'.format(num_new))
+    #logging.debug('Num new from archive: {}'.format(num_new_from_archive))
 
-    existing_count = num_new + len(existing_ids)
+    existing_count = num_new + num_new_from_archive + len(existing_ids)
     logging.info('Total rows: {}, New: {}, Max: {}'.format(
         existing_count, num_new, MAXROWS))
 
