d20cfffbb68b0fc334987caa1285781cfe1340e3
nsuberi <nsuberi@gmail.com>
2018-02-01 10:41:09 -0500

    remove dockerfile dedicated to updates... going to change layer update behavior on the other service anyway as new solution is developed

--- foo_003_fews_new_food_insecurity_layers/start.sh
+++
@@ -1,5 +0,0 @@
-#Change the NAME variable with the name of your script
-NAME=foo_003
-
-docker build -t $NAME --build-arg NAME=$NAME .
-docker run -v $(pwd)/data:/opt/$NAME/data --env-file .env --rm $NAME python main.py
