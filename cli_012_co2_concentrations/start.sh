f10428d07368193f694e950b975f14692eea4767
nsuberi <nsuberi@gmail.com>
2018-02-01 15:01:30 -0500

    clean up changes not related to this pr

--- cli_012_co2_concentrations/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#Change the NAME variable with the name of your script
-NAME=cli_012
-LOG=''
-
-docker build -t $NAME --build-arg NAME=$NAME .
-docker run -v "$(pwd)"/data:/opt/$NAME/data --env-file .env --rm $NAME python main.py
