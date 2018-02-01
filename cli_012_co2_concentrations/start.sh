cfcdfefa30d87d04ecce5b553c964403f2cb8c02
nsuberi <nsuberi@gmail.com>
2018-02-01 14:40:06 -0500

    remove changes to data other than cli_012

--- cli_012_co2_concentrations/start.sh
+++ cli_012_co2_concentrations/start.sh
@@ -2,7 +2,6 @@
 
 #Change the NAME variable with the name of your script
 NAME=cli_012
-LOG=''
 
 docker build -t $NAME --build-arg NAME=$NAME .
 docker run -v "$(pwd)"/data:/opt/$NAME/data --env-file .env --rm $NAME python main.py
