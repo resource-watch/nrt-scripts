bd95a1fe4224c6015cd678f19bb53a00f2a364de
AliyGM <alicia.arenzana@gmail.com>
2018-02-02 09:53:05 +0100

    updated merge

---
+++ cli_012_co2_concentrations/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#Change the NAME variable with the name of your script
+NAME=cli_012
+
+docker build -t $NAME --build-arg NAME=$NAME .
+docker run --log-driver=syslog  --log-opt syslog-address=$LOG --log-opt tag=$NAME -v "$(pwd)"/data:/opt/$NAME/data --env-file .env --rm $NAME python main.py
