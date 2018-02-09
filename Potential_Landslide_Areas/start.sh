d17e266fe7522e7f8ed74831f5311dd3ae7a58bd
nsuberi <nsuberi@gmail.com>
2018-02-09 13:22:56 -0500

    preserve scraping code

--- Potential_Landslide_Areas/start.sh
+++ Potential_Landslide_Areas/start.sh
@@ -1,4 +1,4 @@
 #Change the NAME variable with the name of your script
-NAME=potential_landslides
+NAME=potential_landslides_preserved
 docker build -t $NAME --build-arg NAME=$NAME .
 docker run --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME -v $(pwd)/data:/opt/$NAME/data --env-file .env --rm $NAME
