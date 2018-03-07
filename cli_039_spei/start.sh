b56c2a5fc8e2b80f4553d26f63e145d692af2bcd
nsuberi <nsuberi@gmail.com>
2018-03-07 10:40:16 -0500

    add logging back to cli_039

--- cli_039_spei/start.sh
+++ cli_039_spei/start.sh
@@ -4,5 +4,4 @@
 NAME=cli_039
 
 docker build -t $NAME --build-arg NAME=$NAME .
-docker run -v "$(pwd)"/data:/opt/$NAME/data --env-file .env --rm $NAME python main.py
-#--log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME
+docker run --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME -v "$(pwd)"/data:/opt/$NAME/data --env-file .env --rm $NAME python main.py
