38c6ddde4d3e4b0dc8bffd9f508e0c672fbe7adc
nsuberi <nsuberi@gmail.com>
2018-03-20 22:58:02 -0400

    modify all start.sh using sed -i '' -e '$ d'  <<filename>>, and piping new docker run command without bind mount with >> <<filename>>

--- soc_048_organized_violence_events/start.sh
+++ soc_048_organized_violence_events/start.sh
@@ -5,4 +5,4 @@ NAME=soc_048
 
 docker build -t $NAME --build-arg NAME=$NAME .
 docker run -v $(pwd)/data:/opt/$NAME/data --env-file .env --rm $NAME python main.py
-# --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME 
+docker run --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
