97ba0c05b51c3430a8acea810d3b7df324395829
nsuberi <nsuberi@gmail.com>
2018-03-29 13:30:18 -0400

    remove bind mounts from start.sh, add in python main.py command

--- wat_038_modis_surface_water/start.sh
+++ wat_038_modis_surface_water/start.sh
@@ -6,6 +6,5 @@ LOG=${LOG:-udp://localhost}
 
 docker build -t $NAME --build-arg NAME=$NAME .
 docker run --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME \
-  -v $(pwd)/data:/opt/$NAME/data \
-  -v $(pwd)/mosaics:/opt/$NAME/mosaics \
-  --env-file .env --rm $NAME
+  --env-file .env --rm $NAME \
+  python main.py
