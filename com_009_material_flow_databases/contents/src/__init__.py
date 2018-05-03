f2149f421e4a7781748268d7b16eb3eb7f314753
nsuberi <nsuberi@gmail.com>
2018-05-04 01:41:51 +0800

    wip - add back in countries query to prove this works, small cleanups

--- com_009_material_flow_databases/contents/src/__init__.py
+++ com_009_material_flow_databases/contents/src/__init__.py
@@ -1,9 +1,7 @@
-from __future__ import unicode_literals
-
+# Import libraries
 import os
 import sys
 import logging
-
 from datetime import datetime
 
 import pyodbc
@@ -25,7 +23,6 @@ CARTO_USER = os.environ.get('carto_user')
 CARTO_PASSWORD = os.environ.get('carto_password')
 
 def main():
-    '''Ingest new data into EE and delete old data'''
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
     logging.info('STARTING')
 
@@ -37,19 +34,27 @@ def main():
     cursor = cnxn.cursor()
 
     # Fetch data
+    logging.info("DEMO - run query for countries table to prove this works")
+    before = datetime.now()
+    countries = pd.DataFrame(cursor.execute('SELECT * FROM Countries').fetchall())
+    logging.info('Shape of df is: {}'.format(countries.shape))
+    after = datetime.now()
+    logging.info("Countries query takes {}".format(after-before))
+
+    logging.info("PROCESS THE meat and POTATOES")
     before = datetime.now()
     flowmfa = pd.DataFrame(cursor.execute('SELECT * FROM FlowMFA').fetchall())
-    logging.info('Shape of df is: {}'.format(countries.shape))
+    logging.info('Shape of df is: {}'.format(flowmfa.shape))
     after = datetime.now()
     logging.info("FlowMFA query takes {}".format(after-before))
 
     before = datetime.now()
     flowdetailed = pd.DataFrame(cursor.execute('SELECT * FROM FlowDetailed').fetchall())
-    logging.info('Shape of df is: {}'.format(countries.shape))
+    logging.info('Shape of df is: {}'.format(flowdetailed.shape))
     after = datetime.now()
     logging.info("FlowDetailed query takes {}".format(after-before))
 
-    # Authenticate to carto
+    # Authenticate to carto and upload data
     cc = cartoframes.CartoContext(base_url='https://{}.carto.com/'.format(CARTO_USER),
                                api_key=CARTO_PASSWORD)
 
