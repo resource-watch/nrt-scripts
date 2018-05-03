abb75f9bf44301d75b5cddce27cc833cb7ec1f19
nsuberi <nsuberi@gmail.com>
2018-05-04 01:34:17 +0800

    wip untested - uploading flowmfa and flowdetailed to carto account

--- com_009_material_flow_databases/contents/src/__init__.py
+++ com_009_material_flow_databases/contents/src/__init__.py
@@ -4,48 +4,25 @@ import os
 import sys
 import logging
 
+from datetime import datetime
+
 import pyodbc
+import pandas as pd
+import cartoframes
 
-#from . import queries as q
-q = {}
-q['FlowMFA'] = '''
-SELECT c.Name AS Country, d.Country AS ISOAlpha3, f.Name AS Flow, m2.Name AS MFA13, m.Name AS MFA4, d.Year AS Year, d.Amount AS Amount
-    FROM FlowMFA d LEFT JOIN Country c ON d.Country = c.Code
-	LEFT JOIN Flow f ON d.Flow = f.Code
-	LEFT JOIN MFA13 m2 ON d.MFA13 = m2.Code
-	LEFT JOIN MFA4 m ON d.MFA4 = m.Code
-	ORDER BY Flow, Year, MFA4, Country, MFA13;
-  '''
-
-
-q['FlowDetailed'] = '''
-SELECT d.Year AS Year, c1.Name AS OriginCountry, d.Source AS OriginISOAlpha3, c2.Name AS ConsumerCountry, d.Destination AS ConsumerISOAlpha3, m.Name AS MFA4, p.Name AS ProductGroup, d.Amount AS Amount
-	FROM FlowDetailed d LEFT JOIN Country c1 ON d.Source = c1.Code
-	LEFT JOIN Country c2 ON d.Destination = c2.Code
-	LEFT JOIN MFA4 m ON d.MFA4 = m.Code
-	LEFT JOIN Productgroup p ON d.ProductGroup = p.Code
-	ORDER BY Year, MFA4, ConsumerCountry, ProductGroup, OriginCountry;
-  '''
-
-q['Footprint'] = '''
-SELECT d.Year AS Year, c2.Name AS ConsumerCountry, d.Destination AS ConsumerISOAlpha3, m.Name AS MFA4, sum(d.Amount) AS Amount
-	FROM FlowDetailed d
-	LEFT JOIN Country c2 ON d.Destination = c2.Code
-	LEFT JOIN MFA4 m ON d.MFA4 = m.Code
-	LEFT JOIN Productgroup p ON d.ProductGroup = p.Code
-	GROUP BY Year, MFA4, ConsumerCountry
-	ORDER BY Year, MFA4, ConsumerCountry;
-  '''
-
-# constants for bleaching alerts
-SOURCE_URL = 'vps348928.ovh.net'
-PORT = '5432'
-DATABASE = 'mfa'
-USER = 'mfa'
-PASSWORD = os.environ.get('mfa_db_password')
+# ODBC Connection details -- these can be pulled out into an odbc.ini file
+ODBC_SOURCE_URL = 'vps348928.ovh.net'
+ODBC_PORT = '5432'
+ODBC_DATABASE = 'mfa'
+OBDC_USER = 'mfa'
+ODBC_PASSWORD = os.environ.get('mfa_db_password')
 
 CONNECTION_STRING = 'DRIVER={};SERVER={};PORT={};DATABASE={};UID={};PWD={}'
-cnxnstr = CONNECTION_STRING.format('{PostgreSQL Unicode}',SOURCE_URL, PORT, DATABASE, USER, PASSWORD)
+cnxnstr = CONNECTION_STRING.format('{PostgreSQL Unicode}', ODBC_SOURCE_URL, ODBC_PORT, ODBC_DATABASE, ODBC_USER, ODBC_PASSWORD)
+
+# Carto Connection details
+CARTO_USER = os.environ.get('carto_user')
+CARTO_PASSWORD = os.environ.get('carto_password')
 
 def main():
     '''Ingest new data into EE and delete old data'''
@@ -57,10 +34,26 @@ def main():
     cnxn = pyodbc.connect(cnxnstr, autocommit=True)
     cnxn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
     cnxn.setencoding(encoding='utf-8')
-
     cursor = cnxn.cursor()
 
-    for row in cursor.execute('SELECT * FROM Country').fetchall():
-        logging.info(row)
+    # Fetch data
+    before = datetime.now()
+    flowmfa = pd.DataFrame(cursor.execute('SELECT * FROM FlowMFA').fetchall())
+    logging.info('Shape of df is: {}'.format(countries.shape))
+    after = datetime.now()
+    logging.info("FlowMFA query takes {}".format(after-before))
+
+    before = datetime.now()
+    flowdetailed = pd.DataFrame(cursor.execute('SELECT * FROM FlowDetailed').fetchall())
+    logging.info('Shape of df is: {}'.format(countries.shape))
+    after = datetime.now()
+    logging.info("FlowDetailed query takes {}".format(after-before))
+
+    # Authenticate to carto
+    cc = cartoframes.CartoContext(base_url='https://{}.carto.com/'.format(CARTO_USER),
+                               api_key=CARTO_PASSWORD)
+
+    cc.write(flowmfa, 'com_009_flowmfa', overwrite=True)
+    cc.write(flowdetailed, 'com_009_flowdetailed', overwrite=True)
 
     logging.info('SUCCESS')
