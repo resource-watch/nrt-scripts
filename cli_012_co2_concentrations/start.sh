3b15c5424ccac46c99faa1127087f075ad43d5f2
nsuberi <nsuberi@gmail.com>
2018-05-21 13:07:22 -0400

    clearing cli_012 because not currently nrt

--- cli_012_co2_concentrations/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#Change the NAME variable with the name of your script
-NAME=cli_012
-
-docker build -t $NAME --build-arg NAME=$NAME .
-docker run --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
