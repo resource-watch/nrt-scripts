72db0689b61e89563c8583a155163d777c0fe55c
AliyGM <alicia.arenzana@gmail.com>
2018-07-19 16:15:12 +0200

    change name of the docker container

--- cli_021_snow_cover/start.sh
+++ cli_021_snow_cover/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Change the NAME variable with the name of your script
-NAME=$(basename $(pwd))
+NAME=cli_021
 LOG=${LOG:-udp://localhost}
 
 docker build -t $NAME --build-arg NAME=$NAME .
