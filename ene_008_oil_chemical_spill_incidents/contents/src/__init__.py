47504d3c733ee208a1597bfebc94ca30eed6b3a4
amsnyder <amelia.marie.snyder@gmail.com>
2019-04-09 14:37:53 -0400

    data source formatting has breaks in descriptions, causing data to be incorrectly interpreted. This fixes the problems with reading the data.

--- ene_008_oil_chemical_spill_incidents/contents/src/__init__.py
+++ ene_008_oil_chemical_spill_incidents/contents/src/__init__.py
@@ -11,7 +11,7 @@ import requests
 
 ### Constants
 SOURCE_URL = "https://incidentnews.noaa.gov/raw/incidents.csv"
-CLEAR_TABLE_FIRST = False
+CLEAR_TABLE_FIRST = True
 ENCODING = 'utf-8'
 LOG_LEVEL = logging.INFO
 
@@ -101,38 +101,82 @@ def processData(existing_ids):
     new_rows = []
 
     res = requests.get(SOURCE_URL)
-    csv_reader = csv.reader(res.iter_lines(decode_unicode=True))
+    csv_reader = csv.reader(res.content.decode('utf-8').splitlines(), delimiter=',')
     headers = next(csv_reader, None)
     idx = {k: v for v, k in enumerate(headers)}
 
     for row in csv_reader:
+        #skip empty rows
         if not len(row):
-            break
+            continue
         else:
-            if row[idx['id']] not in existing_ids:
-                new_row = []
-                for field in CARTO_SCHEMA:
-                    if field == 'uid':
-                        new_row.append(row[idx['id']])
-                    elif field == 'the_geom':
-                        # Check for whether valid lat lon provided, will fail if either are ''
-                        lon = float(row[idx['lon']])
-                        lat = float(row[idx['lat']])
-                        if lat and lon:
-                            geometry = {
-                                'type': 'Point',
-                                'coordinates': [lon, lat]
-                            }
-                            new_row.append(geometry)
+            # This data set has some entries with breaks in the last column, which the csv_reader interprets
+            # as an individual row. See if new id can be converted to an integer. If it can, it is probably a
+            # new row.
+            try:
+                int(row[idx['id']])
+                id = row[idx['id']]
+                if id not in existing_ids:
+                    logging.info('new row for {}'.format(id))
+                    new_row = []
+                    for field in CARTO_SCHEMA:
+                        if field == 'uid':
+                            new_row.append(row[idx['id']])
+                        elif field == 'the_geom':
+                            # Check for whether valid lat lon provided, will fail if either are ''
+                            lon = row[idx['lon']]
+                            lat = row[idx['lat']]
+                            if lat and lon:
+                                geometry = {
+                                    'type': 'Point',
+                                    'coordinates': [float(lon), float(lat)]
+                                }
+                                new_row.append(geometry)
+                            else:
+                                logging.debug('No lat long available for this data point - skipping!')
+                                new_row.append(None)
                         else:
-                            logging.debug('No lat long available for this data point - skipping!')
-                            new_row.append(None)
-                    else:
-                        # To fix trouble w/ cartosql not being able to handle '' for numeric:
-                        val = row[idx[field]] if row[idx[field]] != '' else None
-                        new_row.append(val)
-
-                new_rows.append(new_row)
+                            # To fix trouble w/ cartosql not being able to handle '' for numeric:
+                            try:
+                                val = row[idx[field]] if row[idx[field]] != '' else None
+                                new_row.append(val)
+                            except IndexError:
+                                pass
+                    new_rows.append(new_row)
+            #If we can't convert to an integer, the last row probably got cut off.
+            except ValueError:
+                #  Using the id from the last entry, if this id was already in the Carto table, we will skip it
+                if id in existing_ids:
+                    pass
+                # If it is a new id, we need to go fix that row.
+                else:
+                    # If the row is only one item, append the rest of the information to the last description.
+                    if len(row) == 1:
+                        new_rows[-1][-1] = new_rows[-1][-1] + ' ' + row[0].replace('\t', '')
+                    # If several things are in the row, the break was probably mid-row.
+                    elif len(row) > 1 and len(row) < 17:
+                        # finish the last desciption
+                        new_rows[-1][-1] = new_rows[-1][-1] + ' ' + row[0].replace('\t', '')
+                        # append other items to row
+                        new_row = new_rows[-1]
+                        offset_factor = len(new_rows[-1])-1
+                        for field in CARTO_SCHEMA:
+                            if field == 'uid' or field == 'the_geom':
+                                continue
+                            try:
+                                loc=idx[field]-offset_factor
+                                if loc>0:
+                                    val = row[loc] if row[loc] != '' else None
+                                    new_row.append(val)
+                            except IndexError:
+                                pass
+                        new_rows[-1]==new_row
+                        '''
+                        for item in row[1:]:
+                            val = row[idx[field]] if row[idx[field]] != '' else None
+                            new_row.append(val)
+                            new_rows[-1].append(item)
+                        '''
 
     num_new = len(new_rows)
     if num_new:
@@ -148,7 +192,7 @@ def processData(existing_ids):
 def getFieldAsList(table, field, orderBy=''):
     assert isinstance(field, str), 'Field must be a single string'
     r = cartosql.getFields(field, table, order='{}'.format(orderBy),
-                           f='csv')
+                           f='csv', user=os.getenv('CARTO_USER'), key =os.getenv('CARTO_KEY'))
     return(r.text.split('\r\n')[1:-1])
 
 def checkCreateTable(table, schema, id_field, time_field):
@@ -156,7 +200,7 @@ def checkCreateTable(table, schema, id_field, time_field):
     Get existing ids or create table
     Return a list of existing ids in time order
     '''
-    if cartosql.tableExists(table):
+    if cartosql.tableExists(table, user=os.getenv('CARTO_USER'), key =os.getenv('CARTO_KEY')):
         logging.info('Table {} already exists'.format(table))
     else:
         logging.info('Creating Table {}'.format(table))
@@ -188,7 +232,7 @@ def deleteExcessRows(table, max_rows, time_field, max_age=''):
 
 #function to get most recent spill date from table
 def get_most_recent_date(table):
-    r = cartosql.getFields(TIME_FIELD, table, f='csv', post=True)
+    r = cartosql.getFields(TIME_FIELD, table, f='csv', post=True, user=os.getenv('CARTO_USER'), key =os.getenv('CARTO_KEY'))
     dates = r.text.split('\r\n')[1:-1]
     dates.sort()
     most_recent_date = datetime.datetime.strptime(dates[-1], '%Y-%m-%d %H:%M:%S')
