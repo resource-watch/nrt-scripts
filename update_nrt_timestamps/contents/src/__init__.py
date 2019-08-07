ee6eb27b1e1710303998f202a77b0a9a7b36ea73
amsnyder <amelia.marie.snyder@gmail.com>
2019-08-07 14:59:25 -0400

    only_update_if_new

--- update_nrt_timestamps/contents/src/__init__.py
+++ update_nrt_timestamps/contents/src/__init__.py
@@ -19,6 +19,41 @@ DATASET_ID_BY_COLLECTION = {
     'JAXA/GPM_L3/GSMaP/v6/operational': '1e8919fc-c1a8-4814-b819-31cdad17651e'
 }
 
+def getLastUpdate(dataset):
+    apiUrl = 'http://api.resourcewatch.org/v1/dataset/{}'.format(dataset)
+    r = requests.get(apiUrl)
+    lastUpdateString=r.json()['data']['attributes']['dataLastUpdated']
+    nofrag, frag = lastUpdateString.split('.')
+    nofrag_dt = datetime.datetime.strptime(nofrag, "%Y-%m-%dT%H:%M:%S")
+    lastUpdateDT = nofrag_dt.replace(microsecond=int(frag[:-1])*1000)
+    return lastUpdateDT
+
+def getLayerIDs(dataset):
+    apiUrl = 'http://api.resourcewatch.org/v1/dataset/{}?includes=layer'.format(dataset)
+    r = requests.get(apiUrl)
+    layers = r.json()['data']['attributes']['layer']
+    layerIDs =[]
+    for layer in layers:
+        if layer['attributes']['application']==['rw']:
+            layerIDs.append(layer['id'])
+    return layerIDs
+
+def flushTileCache(dataset, layer):
+   """
+  This function will delete the layer cache built for a GEE tiler layer.
+   """
+
+   apiUrl = 'http://api.resourcewatch.org/v1/dataset/{dataset}/layer/{layer}/expire-cache'.format(dataset=dataset, layer=layer)
+   headers = {
+   'Content-Type': 'application/json',
+   'Authorization': os.getenv('apiToken')
+   }
+   try:
+       r = requests.delete(url = apiUrl, headers = headers)
+       logging.info('[Cache tiles deleted]: status code '+str(r.status_code))
+       return r.status_code
+   except Exception as e:
+       logging.error('[lastUpdated]: '+str(e))
 
 def lastUpdateDate(dataset, date):
    apiUrl = 'http://api.resourcewatch.org/v1/dataset/{0}'.format(dataset)
@@ -53,8 +88,12 @@ def main():
     #initialize ee module
     initialize_ee()
     # 1. update data sets in GEE Catalog
+
+    #Check if datasets have been updated
     for collection_name, id in DATASET_ID_BY_COLLECTION.items():
-        logging.info('Updating '+collection_name)
+        #get last update date currently being displayed on RW
+        current_date = getLastUpdate(id)
+        #get most recent date in collection
         #load collection and get most recent asset time stamp
         collection = ee.ImageCollection(collection_name)
         most_recent_asset = collection.sort('system:time_end', opt_ascending=False).first()
@@ -62,8 +101,15 @@ def main():
         most_recent_date_unix = most_recent_asset.get('system:time_end').getInfo()/1000
         #convert to datetime
         most_recent_date = datetime.datetime.fromtimestamp(most_recent_date_unix)
-        # Update data set's last update date on Resource Watch
-        lastUpdateDate(id, most_recent_date)
+        #if our timestamp is not correct, update it and clear the cache
+        if current_date!=most_recent_date:
+            logging.info('Updating ' + collection_name)
+            # Update data set's last update date on Resource Watch
+            lastUpdateDate(id, most_recent_date)
+            layer_ids = getLayerIDs(dataset)
+            for layer_id in layer_ids:
+                #flushTileCache(id, layer_id)
+                logging.info('Cache flushed for layer: '+layer_id)
 
     logging.info('Success for GEE Catalog data sets')
     
@@ -78,7 +124,7 @@ def main():
     sync = json['synchronizations']
 
     for table_name, id in WRIRW_DATASETS.items():
-        logging.info('Updating ' + table_name)
+        current_date = getLastUpdate(id)
         table = next(item for item in sync if item["name"] == table_name)
         # ran_at = The date time at which the table had its contents synched with the source file.
         # updated_at = The date time at which the table had its contents modified.
@@ -86,13 +132,14 @@ def main():
         last_sync = table['updated_at']
         TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
         last_update_time = datetime.datetime.strptime(last_sync, TIME_FORMAT)
-        lastUpdateDate(id, last_update_time)
+        if current_date!=last_update_time:
+            logging.info('Updating ' + table_name)
+            lastUpdateDate(id, last_update_time)
     logging.info('Success for WRI-RW')
 
     # 3. update data sets on GFW Carto account
     GFW_DATASETS = {'gfw_oil_palm': '6e05a9e8-ba07-4e6f-8337-31c5362d07fe',
                     'gfw_wood_fiber': '83de627f-524b-4162-a10c-384dc3e8107a',
-                    'vnp14imgtdl_nrt_global_7d': '20cc5eca-8c63-4c41-8e8e-134dcf1e6d76',
                     'forma_activity': 'e1b40fdd-04f9-43ab-b4f1-d3ceee39fea1'}
 
     url = "https://{account}.carto.com/api/v1/synchronizations/?api_key={API_key}".format(
@@ -103,7 +150,7 @@ def main():
     sync = json['synchronizations']
 
     for table_name, id in GFW_DATASETS.items():
-        logging.info('Updating ' + table_name)
+        current_date = getLastUpdate(id)
         table = next(item for item in sync if item["name"] == table_name)
         # ran_at = The date time at which the table had its contents synced with the source file.
         # updated_at = The date time at which the table had its contents modified.
@@ -111,7 +158,9 @@ def main():
         last_sync = table['updated_at']
         TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
         last_update_time = datetime.datetime.strptime(last_sync, TIME_FORMAT)
-        lastUpdateDate(id, last_update_time)
+        if current_date!=last_update_time:
+            logging.info('Updating ' + table_name)
+            lastUpdateDate(id, last_update_time)
     logging.info('Success for WRI-01')
 
     # 4. update data sets on WRI-RW Carto account
@@ -124,7 +173,7 @@ def main():
     sync = json['synchronizations']
 
     for table_name, id in RWNRT_DATASETS.items():
-        logging.info('Updating ' + table_name)
+        current_date = getLastUpdate(id)
         table = next(item for item in sync if item["name"] == table_name)
         # ran_at = The date time at which the table had its contents synched with the source file.
         # updated_at = The date time at which the table had its contents modified.
@@ -132,5 +181,7 @@ def main():
         last_sync = table['updated_at']
         TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
         last_update_time = datetime.datetime.strptime(last_sync, TIME_FORMAT)
-        lastUpdateDate(id, last_update_time)
+        if current_date!=last_update_time:
+            logging.info('Updating ' + table_name)
+            lastUpdateDate(id, last_update_time)
     logging.info('Success for RW-NRT')
