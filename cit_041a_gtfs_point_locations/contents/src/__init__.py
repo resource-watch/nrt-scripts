60b96b78390fcbc7b792883498f3a17253ae3364
amsnyder <amelia.marie.snyder@gmail.com>
2020-02-07 16:13:12 -0500

    fixes for cit.041

--- cit_041a_gtfs_point_locations/contents/src/__init__.py
+++ cit_041a_gtfs_point_locations/contents/src/__init__.py
@@ -15,31 +15,31 @@ DATA_DIR = 'data'
 DATA_LOCATION_URL = 'https://api.transitfeeds.com/v1/getLocations?key=258e3d67-9c2e-46db-9484-001ce6ff3cc7'
 DATA_URL = 'https://api.transitfeeds.com/v1/getFeeds?key=258e3d67-9c2e-46db-9484-001ce6ff3cc7&location={}'
 
-#Useful links:
-#http://transitfeeds.com/api/
-#https://developers.google.com/transit/gtfs/reference/#pathwaystxt
+# Useful links:
+# http://transitfeeds.com/api/
+# https://developers.google.com/transit/gtfs/reference/#pathwaystxt
 
 
-#Filename for local files
+# Filename for local files
 FILENAME = 'gtfs_points'
 
 # asserting table structure rather than reading from input
-CARTO_TABLE= 'cit_041_gtfs'
+CARTO_TABLE = 'cit_041_gtfs'
 CARTO_SCHEMA = OrderedDict([
-    ('the_geom','geometry'),
+    ('the_geom', 'geometry'),
     ('feed_id', 'numeric'),
-    ('feed_type','text'),
-    ('feed_title','text'),
-    ('loc_id','numeric'),
-    ('ploc_id','numeric'),
-    ('loc_title_l','text'),
-    ('loc_title_s','text'),
-    ('latitude','numeric'),
-    ('longitude','numeric'),
-    ('timestamp_epoch','numeric'),
-    ('ts_latest','timestamp'),
-    ('gtfs_zip','text'),
-    ('gtfs_txt','text')
+    ('feed_type', 'text'),
+    ('feed_title', 'text'),
+    ('loc_id', 'numeric'),
+    ('ploc_id', 'numeric'),
+    ('loc_title_l', 'text'),
+    ('loc_title_s', 'text'),
+    ('latitude', 'numeric'),
+    ('longitude', 'numeric'),
+    ('timestamp_epoch', 'numeric'),
+    ('ts_latest', 'timestamp'),
+    ('gtfs_zip', 'text'),
+    ('gtfs_txt', 'text')
 ])
 INPUT_DATE_FORMAT = '%Y%m%d'
 DATE_FORMAT = '%Y-%m-%d'
@@ -54,24 +54,27 @@ CARTO_KEY = os.environ.get('CARTO_KEY')
 ###
 
 DATASET_ID = 'ca607a0d-3ab9-4b22-b4fe-5c43b17e47c4'
+
+
 def lastUpdateDate(dataset, date):
-   apiUrl = 'http://api.resourcewatch.org/v1/dataset/{0}'.format(dataset)
-   headers = {
-   'Content-Type': 'application/json',
-   'Authorization': os.getenv('apiToken')
-   }
-   body = {
-       "dataLastUpdated": date.isoformat()
-   }
-   try:
-       r = requests.patch(url = apiUrl, json = body, headers = headers)
-       logging.info('[lastUpdated]: SUCCESS, '+ date.isoformat() +' status code '+str(r.status_code))
-       return 0
-   except Exception as e:
-       logging.info('[lastUpdated]: '+str(e))
+    apiUrl = 'http://api.resourcewatch.org/v1/dataset/{0}'.format(dataset)
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': os.getenv('apiToken')
+    }
+    body = {
+        "dataLastUpdated": date.isoformat()
+    }
+    try:
+        r = requests.patch(url=apiUrl, json=body, headers=headers)
+        logging.info('[lastUpdated]: SUCCESS, ' + date.isoformat() + ' status code ' + str(r.status_code))
+        return 0
+    except Exception as e:
+        logging.info('[lastUpdated]: ' + str(e))
+
 
 def get_most_recent_date(table):
-    #r = cartosql.getFields(TIME_FIELD, table, f='csv', post=True)
+    # r = cartosql.getFields(TIME_FIELD, table, f='csv', post=True)
     r = getFields(TIME_FIELD, table, f='csv', post=True)
     dates = r.text.split('\r\n')[1:-1]
     dates.sort()
@@ -81,14 +84,17 @@ def get_most_recent_date(table):
 
 def formatDate(date):
     """ Parse input date string and write in output date format """
-    return datetime.datetime.strptime(date, INPUT_DATE_FORMAT)\
-                            .strftime(DATE_FORMAT)
+    return datetime.datetime.strptime(date, INPUT_DATE_FORMAT) \
+        .strftime(DATE_FORMAT)
+
+
 def getFilename(date):
     '''get filename from datestamp CHECK FILE TYPE'''
     return os.path.join(DATA_DIR, '{}.csv'.format(
         FILENAME.format(date=date.strftime('%Y%m%d'))))
 
-def getGeom(lon,lat):
+
+def getGeom(lon, lat):
     '''Define point geometry from latitude and longitude'''
     geometry = {
         'type': 'Point',
@@ -96,25 +102,28 @@ def getGeom(lon,lat):
     }
     return geometry
 
+
 def convert_time_since_epoch(timestamp):
     '''Function to convert seconds since the epoch to human readable time'''
     value = datetime.datetime.fromtimestamp(timestamp)
     return value.strftime('%Y-%m-%d')
-    
+
+
 def location():
     '''Function to grab the unique location id (uids) from the locations api.'''
     logging.info('Fetching location ids')
-    r=requests.get(DATA_LOCATION_URL)
-    json_obj=r.json()
-    json_obj_list=json_obj['results']
-    json_obj_list_get=json_obj_list.get('locations')
-    location_id=[]
+    r = requests.get(DATA_LOCATION_URL)
+    json_obj = r.json()
+    json_obj_list = json_obj['results']
+    json_obj_list_get = json_obj_list.get('locations')
+    location_id = []
     for dict in json_obj_list_get:
-        x=dict.get('id')
+        x = dict.get('id')
         location_id.append(x)
-        logging.info('Location Ids Collected')
+    logging.info('Location Ids Collected')
     return location_id
-  
+
+
 def feeds():
     '''Function to use the uids to obtain the feed information and put them into a pandas dataframe with all the dictionaries unpacked'''
     feed_list = []
@@ -133,12 +142,13 @@ def feeds():
     df_2 = pd.concat([df_3.drop(['l'], axis=1), df_3['l'].apply(pd.Series)], axis=1)
     df_1 = pd.concat([df_2.drop(['latest'], axis=1), df_2['latest'].apply(pd.Series)], axis=1)
     df = pd.concat([df_1.drop(['u'], axis=1), df_1['u'].apply(pd.Series)], axis=1)
-    
-    #Original columns = 'id', 'ty', 't', 'id', 'pid', 't', 'n', 'lat', 'lng', 0, 'ts', 'd', 'i', described in API documentation http://transitfeeds.com/api/swagger/#!/default/getFeeds
+
+    # Original columns = 'id', 'ty', 't', 'id', 'pid', 't', 'n', 'lat', 'lng', 0, 'ts', 'd', 'i', described in API documentation http://transitfeeds.com/api/swagger/#!/default/getFeeds
     df = df.dropna(axis=1, how='all')
-    new_columns = ['feed_id','feed_type','feed_title','loc_id','ploc_id','loc_title_l','loc_title_s','latitude','longitude','timestamp_epoch','gtfs_zip','gtfs_txt']
-    df.columns = new_columns        
-    df['the_geom'] = df.apply(lambda row: getGeom(row['longitude'],row['latitude']),axis=1)
+    new_columns = ['feed_id', 'feed_type', 'feed_title', 'loc_id', 'ploc_id', 'loc_title_l', 'loc_title_s', 'latitude',
+                   'longitude', 'timestamp_epoch', 'gtfs_zip', 'gtfs_txt']
+    df.columns = new_columns
+    df['the_geom'] = df.apply(lambda row: getGeom(row['longitude'], row['latitude']), axis=1)
     df['timestamp_epoch'] = np.nan_to_num(df['timestamp_epoch'].values)
     df['ts_latest'] = [convert_time_since_epoch(x) for x in df['timestamp_epoch'].values]
     return df
@@ -152,12 +162,10 @@ def processData():
     success = False
     tries = 0
     df = None
-    while tries < MAX_TRIES and success==False:
+    while tries < MAX_TRIES and success == False:
         logging.info('Try running feeds, try number = {}'.format(tries))
         try:
             df = feeds()
-            logging.info(df.columns)
-            logging.info(df.iloc[0])
             success = True
         except Exception as inst:
             logging.info(inst)
@@ -170,14 +178,15 @@ def processData():
         if not cartosql.tableExists(CARTO_TABLE):
             logging.info('Table {} does not exist'.format(CARTO_TABLE))
             cartosql.createTable(CARTO_TABLE, CARTO_SCHEMA)
-            #Send dataframe to Carto
+            # Send dataframe to Carto
             logging.info('Writing to Carto')
             cc = cartoframes.CartoContext(base_url="https://{user}.carto.com/".format(user=CARTO_USER),
                                           api_key=CARTO_KEY)
             cc.write(df, CARTO_TABLE, overwrite=True, privacy='public')
         else:
-            cartosql.deleteRows(CARTO_TABLE, 'cartodb_id IS NOT NULL', user=os.getenv('CARTO_USER'), key=os.getenv('CARTO_KEY'))
-            #Send dataframe to Carto
+            cartosql.deleteRows(CARTO_TABLE, 'cartodb_id IS NOT NULL', user=os.getenv('CARTO_USER'),
+                                key=os.getenv('CARTO_KEY'))
+            # Send dataframe to Carto
             logging.info('Writing to Carto')
             cc = cartoframes.CartoContext(base_url="https://{user}.carto.com/".format(user=CARTO_USER),
                                           api_key=CARTO_KEY)
@@ -189,5 +198,5 @@ def main():
     logging.info('STARTING')
     processData()
     # Push update date
-    #lastUpdateDate(DATASET_ID, datetime.datetime.now())
+    # lastUpdateDate(DATASET_ID, datetime.datetime.now())
     logging.info('SUCCESS')
