fd38d612509f13f60be8ae10071e05b278746d48
Taufiq Rashid <43042654+Taufiq06@users.noreply.github.com>
2020-04-01 10:18:14 -0400

    Update cli_021_snow_cover/contents/src/__init__.py
    Co-Authored-By: Amelia <30877272+amsnyder@users.noreply.github.com>

--- cli_021_snow_cover/contents/src/__init__.py
+++ cli_021_snow_cover/contents/src/__init__.py
@@ -236,7 +236,7 @@ def getNewDates(exclude_dates):
     '''
     Get new dates we want to try to fetch data for
     INPUT   exclude_dates: list of dates that we already have in GEE, in the format of the DATE_FORMAT variable (list of strings)
-    RETURN  new_dates: list of new dates we want to try to get in the format of the DATE_FORMAT variable (list of strings)
+    RETURN  new_dates: list of new dates we want to try to get, in the format of the DATE_FORMAT variable (list of strings)
     '''
     # create empty list to store dates we want to fetch
     new_dates = []
