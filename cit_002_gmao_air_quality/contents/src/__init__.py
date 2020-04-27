099cd350a3eb16440a1db2232cee10a30078a10f
Taufiq Rashid <43042654+Taufiq06@users.noreply.github.com>
2020-04-27 11:23:14 -0400

    Update cit_002_gmao_air_quality/contents/src/__init__.py
    Co-Authored-By: Amelia <30877272+amsnyder@users.noreply.github.com>

--- cit_002_gmao_air_quality/contents/src/__init__.py
+++ cit_002_gmao_air_quality/contents/src/__init__.py
@@ -34,7 +34,6 @@ VARS = ['NO2', 'O3', 'PM25_RH35_GCC']
 CONVERSION_FACTORS = {
     'NO2': 1e9, # mol/mol to ppb
     'O3': 1e9, # mol/mol to ppb
-    # keep original units
     'PM25_RH35_GCC': 1, 
 }
 
