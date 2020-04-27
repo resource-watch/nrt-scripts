e3d559797e8dc32e8ebfde2a07f6f30856fed97e
Taufiq Rashid <43042654+Taufiq06@users.noreply.github.com>
2020-04-27 12:24:44 -0400

    Update cli_039_spei/contents/src/__init__.py
    Co-Authored-By: Amelia <30877272+amsnyder@users.noreply.github.com>

--- cli_039_spei/contents/src/__init__.py
+++ cli_039_spei/contents/src/__init__.py
@@ -342,7 +342,6 @@ def extract_subdata_by_date(nc_file, lag, dtype, nodata, available_dates, target
         # Extract data from netcdf for the available date
         data_tmp = nc[VAR_NAME][date_ix,:,:]
         # change center point of data by switching left and right side of data matrix
-        data = np.zeros(data_tmp.shape)
         data[:,:90] = data_tmp[:,90:]
         data[:,90:] = data_tmp[:,:90]
 
@@ -533,4 +532,3 @@ def main():
     updateResourceWatch()
 
     logging.info('SUCCESS')
-
