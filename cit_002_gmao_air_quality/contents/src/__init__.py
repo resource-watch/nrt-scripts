cec63b1eaac38c1f14c0f89725eccf9bc934125d
amsnyder <amelia.marie.snyder@gmail.com>
2020-05-22 01:01:26 -0400

    revert ee

--- cit_002_gmao_air_quality/contents/src/__init__.py
+++ cit_002_gmao_air_quality/contents/src/__init__.py
@@ -738,6 +738,19 @@ def listAllCollections(var, period):
                 all_assets.append(item['id'])
     return all_assets
 
+def initialize_ee():
+    '''
+    Initialize ee module
+    '''
+    # get GEE credentials from env file
+    GEE_JSON = os.environ.get("GEE_JSON")
+    _CREDENTIAL_FILE = 'credentials.json'
+    GEE_SERVICE_ACCOUNT = os.environ.get("GEE_SERVICE_ACCOUNT")
+    with open(_CREDENTIAL_FILE, 'w') as f:
+        f.write(GEE_JSON)
+    auth = ee.ServiceAccountCredentials(GEE_SERVICE_ACCOUNT, _CREDENTIAL_FILE)
+    ee.Initialize(auth)
+
 def create_headers():
     '''
     Create headers to perform authorized actions on API
@@ -904,9 +917,8 @@ def main():
     logging.info('STARTING')
 
     # Initialize eeUtil and ee modules
-    auth = ee.ServiceAccountCredentials(os.getenv('GEE_SERVICE_ACCOUNT'), key_data=os.getenv('GEE_JSON'))
-    ee.Initialize(auth)
     eeUtil.initJson()
+    initialize_ee()
 
     '''
     Process Historical Data
