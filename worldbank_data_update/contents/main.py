b6fcabee70c5954d1510af71f250117033d3495c
amsnyder <amelia.marie.snyder@gmail.com>
2020-06-04 09:16:58 -0400

    chmod python module

--- worldbank_data_update/contents/main.py
+++ worldbank_data_update/contents/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 if __name__ == '__main__':
-    import src
     from src import update_worldbank_data_on_carto
     from src import update_worldbank_layers_on_rw
     update_worldbank_data_on_carto.main()
