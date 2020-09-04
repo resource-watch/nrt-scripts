f900418e730079c2cf204aa3db770a6a408a5eef
root <root@4BJCLQ2.internal.wri.org>
2020-09-04 10:48:50 -0400

    remove cit.004

--- cit_004_city_aq/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#Change the NAME variable with the name of your script
-NAME=cit_004_city_aq
-LOG=${LOG:-udp://localhost}
-
-docker build -t $NAME --build-arg NAME=$NAME .
-docker run --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
