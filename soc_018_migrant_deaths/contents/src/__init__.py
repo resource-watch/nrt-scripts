b75848bd3b6ab7d60b3f1faaa07d197494f4f8bd
Taufiq Rashid <43042654+Taufiq06@users.noreply.github.com>
2020-10-12 13:49:39 -0400

    soc_018_add_dates

--- soc_018_migrant_deaths/contents/src/__init__.py
+++ soc_018_migrant_deaths/contents/src/__init__.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 import datetime
 import cartosql
 import os
+import json
 
 '''
 ---------------------------------Important Note--------------------------------------------------
@@ -303,6 +304,81 @@ def deleteExcessRows(table, max_rows, time_field, max_age=''):
 
     return(num_dropped)
 
+def create_headers():
+    '''
+    Create headers to perform authorized actions on API
+
+    '''
+    return {
+        'Content-Type': "application/json",
+        'Authorization': "{}".format(os.getenv('apiToken')),
+    }
+
+def pull_layers_from_API(dataset_id):
+    '''
+    Pull dictionary of current layers from API
+    INPUT   dataset_id: Resource Watch API dataset ID (string)
+    RETURN  layer_dict: dictionary of layers (dictionary of strings)
+    '''
+    # generate url to access layer configs for this dataset in back office
+    rw_api_url = 'https://api.resourcewatch.org/v1/dataset/{}/layer?page[size]=100'.format(dataset_id)
+    # request data
+    r = requests.get(rw_api_url)
+    # convert response into json and make dictionary of layers
+    layer_dict = json.loads(r.content.decode('utf-8'))['data']
+    return layer_dict
+
+def update_layer(layer, new_date):
+    '''
+    Update layers in Resource Watch back office.
+    INPUT   layer: layer that will be updated (string)
+            new_date: date of asset to be shown in this layer (datetime)
+    '''
+    # get current layer titile
+    title = layer['attributes']['name'] 
+    # get layer description
+    lyr_dscrptn = layer['attributes']['description']   
+    
+    # if we are processing the layer that shows latest 60 days data
+    if lyr_dscrptn.endswith('past 60 days.'):
+        # get current date being used from title by string manupulation
+        old_date_text = title.split(' Migrant')[0]
+        # get text for new date end which will be the current date
+        new_date_end = new_date.strftime("%B %d, %Y")
+        # get most recent starting date, 60 days ago
+        new_date_start = (new_date - datetime.timedelta(days=59))
+        new_date_start = datetime.datetime.strftime(new_date_start, "%B %d, %Y")
+        # construct new date range by joining new start date and new end date
+        new_date_text = new_date_start + ' - ' + new_date_end
+    # if we are processing the layer that shows 2014-present data
+    elif lyr_dscrptn.endswith('2014 to the present.'):    
+        # get current date being used from title by string manupulation
+        old_date_text = title.split(' Number')[0].split(' - ')[1]
+        # get text for new date end which will be the current date
+        new_date_text = new_date.strftime("%B %d, %Y")
+    
+    # replace date in layer's title with new date
+    layer['attributes']['name'] = layer['attributes']['name'].replace(old_date_text, new_date_text)
+
+    # send patch to API to replace layers
+    # generate url to patch layer
+    rw_api_url_layer = "https://api.resourcewatch.org/v1/dataset/{dataset_id}/layer/{layer_id}".format(
+        dataset_id=layer['attributes']['dataset'], layer_id=layer['id'])
+    # create payload with new title and layer configuration
+    payload = {
+        'application': ['rw'],
+        'name': layer['attributes']['name']
+    }
+    # patch API with updates
+    r = requests.request('PATCH', rw_api_url_layer, data=json.dumps(payload), headers=create_headers())
+    # check response
+    # if we get a 200, the layers have been replaced
+    # if we get a 504 (gateway timeout) - the layers are still being replaced, but it worked
+    if r.ok or r.status_code==504:
+        logging.info('Layer replaced: {}'.format(layer['id']))
+    else:
+        logging.error('Error replacing layer: {} ({})'.format(layer['id'], r.status_code))
+        
 def updateResourceWatch(num_new):
     '''
     This function should update Resource Watch to reflect the new data.
@@ -314,8 +390,14 @@ def updateResourceWatch(num_new):
         # Update dataset's last update date on Resource Watch
         most_recent_date = datetime.datetime.utcnow()
         lastUpdateDate(DATASET_ID, most_recent_date)
-
-    # Update the dates on layer legends - TO BE ADDED IN FUTURE
+        # Update the dates on layer legends
+        logging.info('Updating {}'.format(CARTO_TABLE))
+        # pull dictionary of current layers from API
+        layer_dict = pull_layers_from_API(DATASET_ID)
+        # go through each layer, pull the definition and update
+        for layer in layer_dict:
+            # replace layer title with new dates
+            update_layer(layer, most_recent_date)
 
 def main():
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
