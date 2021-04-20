647d2db10ffbec42b7c7778290ba0689e2cbcd38
Yujing Wu <49011074+adawyj97@users.noreply.github.com>
2021-04-20 11:02:25 -0400

    update docker run parameters

--- bio_007_rw2_world_database_on_protected_areas/start.sh
+++ bio_007_rw2_world_database_on_protected_areas/start.sh
@@ -5,12 +5,12 @@ LOG=${LOG:-udp://localhost}
 docker build -t $NAME --no-cache --build-arg NAME=$NAME .
 # --m is used to limit the memory usage of this script to prevent memory errors
 docker run \
-    -m 1600m \
+    -m 1700m \
+    --oom-kill-disable \
     --log-driver=syslog \
     --log-opt syslog-address=$LOG \
     --log-opt tag=$NAME \
     --env-file .env \
-    --rm $NAME \
     python main.py
 
 
