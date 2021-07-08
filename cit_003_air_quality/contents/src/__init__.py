b4244dc6e3d42c44f3ca11e6baa3fb1d03f61ef3
weiqi-tori <zhouwq@umich.edu>
2021-07-08 17:04:36 -0400

    add comments

--- cit_003_air_quality/contents/src/__init__.py
+++ cit_003_air_quality/contents/src/__init__.py
@@ -14,17 +14,20 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 ### Constants
 DATA_DIR = 'data'
-# max page size = 10000
+# url at which the data can be downloaded
 DATA_URL = 'https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements?date_from={date_from}T{hour}%3A{minute}%3A{second}&date_to={date_to}T{hour}%3A{minute}%3A{second}&limit=3000&page={page}&sort=desc&has_geo=true&parameter={parameter}&order_by=datetime&sensorType=reference%20grade'
-# always check first 15 pages
+# always check first 20 pages
 MIN_PAGES = 15
+# max page size = 100
 MAX_PAGES = 100
 
 # how long to wait before trying to get data again incase of failure
-WAIT_TIME = 60
+WAIT_TIME = 30
 
 # asserting table structure rather than reading from input
 PARAMS = ('pm25', 'pm10', 'so2', 'no2', 'o3', 'co', 'bc')
+
+# the name of the seven carto tables to store the data 
 CARTO_TABLES = {
     'pm25':'cit_003a_air_quality_pm25',
     'pm10':'cit_003b_air_quality_pm10',
@@ -34,6 +37,9 @@ CARTO_TABLES = {
     'co':'cit_003f_air_quality_co',
     'bc':'cit_003g_air_quality_bc'
 }
+# column names and types for data table
+# column names should be lowercase
+# column types should be one of the following: geometry, text, numeric, timestamp
 CARTO_SCHEMA = OrderedDict([
     ("the_geom", "geometry"),
     ("_UID", "text"),
@@ -47,7 +53,9 @@ CARTO_SCHEMA = OrderedDict([
     ("attribution", "text"),
     ("ppm", "numeric")
 ])
+# the name of the seven carto tables to store the geometry data 
 CARTO_GEOM_TABLE = 'cit_003loc_air_quality'
+# column names and types for the geometry data table
 CARTO_GEOM_SCHEMA = OrderedDict([
     ("the_geom", "geometry"),
     ("_UID", "text"),
@@ -56,18 +64,20 @@ CARTO_GEOM_SCHEMA = OrderedDict([
     ("country", "text")
 ])
 
-
+# column of table that can be used as a unique ID (UID)
 UID_FIELD = '_UID'
+# column of table that can be used as a timestamp
 TIME_FIELD = 'utc'
 
+# carto username and API key for account where we will store the data
 CARTO_USER = os.environ.get('CARTO_USER')
 CARTO_KEY = os.environ.get('CARTO_KEY')
 
-# Limit to 500000 rows / 30 days
+# limit to 500000 rows / 30 days
 MAXROWS = 500000
 MAXAGE = datetime.datetime.now() - datetime.timedelta(days=30)
 
-# conversions
+# conversion units and parameters
 UGM3 = ["\u00b5g/m\u00b3", "ug/m3"]
 MOL_WEIGHTS = {
     'so2': 64,
@@ -76,6 +86,10 @@ MOL_WEIGHTS = {
     'co': 28
 }
 
+# Resource Watch dataset API ID
+# Important! Before testing this script:
+# Please change this ID OR comment out the getLayerIDs(DATASET_ID) function in the script below
+# Failing to do so will overwrite the last update date on a different dataset on Resource Watch
 DATASET_ID = {
     'pm25':'ae7227d1-8779-4ca4-a2ce-3c87d53c63f6',
     'pm10':'7c36dbb7-6685-4dc7-b285-7476db05cd5e',
@@ -86,49 +100,137 @@ DATASET_ID = {
     'bc':'0c3ed5b9-94b4-4fc5-9208-bf749f0a5052'
 }
 
-def lastUpdateDate(dataset, date):
-   apiUrl = 'http://api.resourcewatch.org/v1/dataset/{0}'.format(dataset)
-   headers = {
-   'Content-Type': 'application/json',
-   'Authorization': os.getenv('apiToken')
-   }
-   body = {
-       "dataLastUpdated": date.isoformat()
-   }
-   try:
-       r = requests.patch(url = apiUrl, json = body, headers = headers)
-       logging.info('[lastUpdated]: SUCCESS, '+ date.isoformat() +' status code '+str(r.status_code))
-       return 0
-   except Exception as e:
-       logging.error('[lastUpdated]: '+str(e))
+'''
+FUNCTIONS FOR ALL DATASETS
 
+The functions below must go in every near real-time script.
+Their format should not need to be changed.
+'''
+def lastUpdateDate(dataset, date):
+    '''
+    Given a Resource Watch dataset's API ID and a datetime,
+    this function will update the dataset's 'last update date' on the API with the given datetime
+    INPUT   dataset: Resource Watch API dataset ID (string)
+            date: date to set as the 'last update date' for the input dataset (datetime)
+    '''
+    # generate the API url for this dataset
+    apiUrl = 'http://api.resourcewatch.org/v1/dataset/{0}'.format(dataset)
+    # create headers to send with the request to update the 'last update date'
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': os.getenv('apiToken')
+    }
+    # create the json data to send in the request
+    body = {
+        "dataLastUpdated": date.isoformat()
+    }
+    # send the request
+    try:
+        r = requests.patch(url = apiUrl, json = body, headers = headers)
+        logging.info('[lastUpdated]: SUCCESS, '+ date.isoformat() +' status code '+str(r.status_code))
+        return 0
+    except Exception as e:
+        logging.error('[lastUpdated]: '+str(e))
+
+def delete_local():
+    '''
+    Delete all files and folders in Docker container's data directory
+    '''
+    try:
+        # for each object in the data directory
+        for f in os.listdir(DATA_DIR):
+            # try to remove it as a file
+            try:
+                logging.info('Removing {}'.format(f))
+                os.remove(DATA_DIR+'/'+f)
+            # if it is not a file, remove it as a folder
+            except:
+                shutil.rmtree(f, ignore_errors=True)
+    except NameError:
+        logging.info('No local files to clean.')
+
+'''
+FUNCTIONS FOR CARTO DATASETS
+
+The functions below must go in every near real-time script for a Carto dataset.
+Their format should not need to be changed.
+'''
+def checkCreateTable(table, schema, id_field, time_field=''):
+    '''
+    Create the table if it does not exist, and pull list of IDs already in the table if it does
+    INPUT   table: Carto table to check or create (string)
+            schema: dictionary of column names and types, used if we are creating the table for the first time (dictionary)
+            id_field: name of column that we want to use as a unique ID for this table; this will be used to compare the
+                    source data to the our table each time we run the script so that we only have to pull data we
+                    haven't previously uploaded (string)
+            time_field:  optional, name of column that will store datetime information (string)
+    RETURN  list of existing IDs in the table, pulled from the id_field column (list of strings)
+    '''
+    # check it the table already exists in Carto
+    if cartosql.tableExists(table):
+        # if the table does exist, get a list of all the values in the id_field column
+        logging.info('Fetching existing IDs')
+        r = cartosql.getFields(id_field, table, f='csv', post=True)
+        # turn the response into a list of strings, removing the first and last entries (header and an empty space at end)
+        return r.text.split('\r\n')[1:-1]
+    else:
+        # if the table does not exist, create it with columns based on the schema input
+        logging.info('Table {} does not exist, creating'.format(table))
+        cartosql.createTable(table, schema)
+        # if a unique ID field is specified, set it as a unique index in the Carto table; when you upload data, Carto
+        # will ensure no two rows have the same entry in this column and return an error if you try to upload a row with
+        # a duplicate unique ID
+        cartosql.createIndex(table, id_field, unique=True)
+        # if a time_field is specified, set it as an index in the Carto table; this is not a unique index
+        if time_field:
+            cartosql.createIndex(table, time_field)
+        # return an empty list because there are no IDs in the new table yet
+        return []
+
+'''
+FUNCTIONS FOR THIS DATASET
+
+The functions below have been tailored to this specific dataset.
+They should all be checked because their format likely will need to be changed.
+'''
 def convert(param, unit, value):
+    '''
+    Conversion
+    '''
     if param in MOL_WEIGHTS.keys() and unit in UGM3:
         return convert_ugm3_ppm(value, MOL_WEIGHTS[param])
     return value
 
-
 def convert_ugm3_ppm(ugm3, mol, T=0, P=101.325):
-    # ideal gas conversion
+    '''
+    Ideal gas conversion
+    '''
     K = 273.15    # 0C
     Atm = 101.325 # kPa
     return float(ugm3)/mol * 22.414 * (T+K)/K * Atm/P / 1000
 
-
-# Generate UID
 def genUID(obs):
+    '''
+    Generate UID
+    '''
     # location should be unique, plus measurement timestamp
     id_str = '{}_{}'.format(obs['location'], obs['date']['utc'])
     return hashlib.md5(id_str.encode('utf8')).hexdigest()
 
-
-# Generate UID for location
 def genLocID(obs):
+    '''
+    Generate UID for location
+    '''
     return hashlib.md5(obs['location'].encode('utf8')).hexdigest()
 
-
-# Parse OpenAQ fields
 def parseFields(obs, uid, fields):
+    '''
+    Parse OpenAQ fields
+    INPUT   obj: object in requested json
+            uid: Unique ID (UID)
+            fields: column names for data table
+    OUTPUT  data saved as Carto table rows
+    '''
     row = []
     for field in fields:
         if field == 'the_geom':
@@ -163,24 +265,14 @@ def parseFields(obs, uid, fields):
             row.append(obs[field])
     return row
 
-
-def checkCreateTable(table, schema, id_field, time_field=''):
-    '''Get existing ids or create table'''
-    if cartosql.tableExists(table):
-        logging.info('Fetching existing IDs')
-        r = cartosql.getFields(id_field, table, f='csv', post=True)
-        return r.text.split('\r\n')[1:-1]
-    else:
-        logging.info('Table {} does not exist, creating'.format(table))
-        cartosql.createTable(table, schema)
-        cartosql.createIndex(table, id_field, unique=True)
-        if time_field:
-            cartosql.createIndex(table, time_field)
-    return []
-
-
 def deleteExcessRows(table, max_rows, time_field, max_age=''):
-    '''Delete excess rows by age or count'''
+    '''
+    Delete excess rows by age or count
+    INPUT   table: Carto table to check
+            max_rows: row limitation
+            time_field: column of table that can be used as a timestamp
+            max_age: age limitation
+    '''
     num_dropped = 0
     if isinstance(max_age, datetime.datetime):
         max_age = max_age.isoformat()
@@ -203,6 +295,11 @@ def deleteExcessRows(table, max_rows, time_field, max_age=''):
         logging.info('Dropped {} old rows from {}'.format(num_dropped, table))
 
 def get_most_recent_date(param):
+    '''
+    Get the most recent date of records in the Carto table
+    INPUT   param: parameter to choose the Carto table
+    OUTPUT  most_recent_date: get the most recent date
+    '''
     r = cartosql.getFields(TIME_FIELD, CARTO_TABLES[param], f='csv', post=True)
     dates = r.text.split('\r\n')[1:-1]
     dates.sort()
@@ -215,7 +312,6 @@ def get_most_recent_date(param):
 def create_headers():
     '''
     Create headers to perform authorized actions on API
-
     '''
     return {
         'Content-Type': "application/json",
@@ -300,15 +396,16 @@ def main():
     loc_ids = checkCreateTable(CARTO_GEOM_TABLE, CARTO_GEOM_SCHEMA, UID_FIELD)
 
     # 2. Iterively fetch, parse and post new data
-    # this is done all together because OpenAQ endpoint filter by parameter
-    # doesn't work
     new_counts = dict(((param, 0) for param in PARAMS))
 
     for param in PARAMS:
         page = 1
         retries = 0
+        # set date_from to 24 hours ago
         date_from = (datetime.datetime.utcnow()-datetime.timedelta(hours=24)).strftime("%Y-%m-%d")
+        # set date_to to current date
         date_to = datetime.datetime.utcnow().strftime("%Y-%m-%d")
+        # get current hour, minute, and second
         hour = datetime.datetime.utcnow().hour
         minute = datetime.datetime.utcnow().minute
         second = datetime.datetime.utcnow().second
@@ -370,10 +467,12 @@ def main():
             # failed to read ['results']
             except Exception as e:
                 logging.info('Failed to read results. Waiting for {} seconds before trying again.'.format(WAIT_TIME))
-                time.sleep(30)
+                time.sleep(WAIT_TIME)
                 retries += 1
                 page -= 1
+                # maximum attempts to make
                 if retries > 5:
+                    # break
                     raise(e)
 
         # 3. Remove old observations
@@ -393,5 +492,8 @@ def main():
         for layer in layer_dict:
             # replace layer description with new dates
             update_layer(layer)
+    
+    # Delete local files in Docker container
+    delete_local()
 
     logging.info('SUCCESS')
