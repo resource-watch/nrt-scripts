79439fce9060934c2ba63b32dfa0ba80e5faa9e8
weiqi-tori <zhouwq@umich.edu>
2022-05-01 21:17:40 +0800

    update 2022 data

--- foo_053_alerts_price_spikes/contents/src/__init__.py
+++ foo_053_alerts_price_spikes/contents/src/__init__.py
@@ -373,7 +373,7 @@ def parseAlps(market_data, existing_alps):
     # create an empty list to store data from this row
     row = []
     # if the unique id doesn't already exist in our Carto table
-    if uid not in existing_alps:
+    if uid not in existing_alps and date > datetime.datetime.strptime('01/01/2022', "%d/%m/%Y"):
         # append the id to existing_alps list
         existing_alps.append(uid)
 
