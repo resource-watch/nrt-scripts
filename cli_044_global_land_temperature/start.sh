d2f5536aa7edd6699754d179ad0a7669bd199121
weiqi-tori <zhouwq@umich.edu>
2025-04-02 12:49:06 +0800

    set local host

--- cli_044_global_land_temperature/start.sh
+++ cli_044_global_land_temperature/start.sh
@@ -1,5 +1,6 @@
 #Change the NAME variable with the name of your script
 NAME=$(basename $(pwd))
+LOG=${LOG:-udp://localhost}
 
 docker build -t $NAME --build-arg NAME=$NAME .
-docker run  --network=host --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
+docker run  --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
