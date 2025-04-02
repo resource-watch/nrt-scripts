2aa7a461d65f94090c186d9077655a41905163dd
weiqi-tori <zhouwq@umich.edu>
2025-04-02 12:41:42 +0800

    set host

--- cli_044_global_land_temperature/start.sh
+++ cli_044_global_land_temperature/start.sh
@@ -2,4 +2,4 @@
 NAME=$(basename $(pwd))
 
 docker build -t $NAME --build-arg NAME=$NAME .
-docker run --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
+docker run  --network=host --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME --env-file .env --rm $NAME python main.py
